5bdc6ab9577d3d2988ed01d5291fa69728b1a6e0
bndeepankan <bn.deepankan@gmail.com>
2020-07-04 13:28:17 +0530

    solutions for problem 10

--- problems10/k_medoids.py
+++ problems10/k_medoids.py
@@ -1,6 +1,7 @@
 from GameAI.problems07.make_graph import *
 from GameAI.problems07.dijkstra import dijkstra
 import numpy as np
+from colorama import Fore
 
 
 def k_medoids(G, k, distMap, tmax = 100):
@@ -44,20 +45,20 @@ if __name__ == '__main__':
     graph = obj.build_nx_graph(obj.succDict, obj.nodeDict)  # Create the graph from the labels and location
 
     nodes = max(graph.nodes)
-    distMat = [[-1 for x in range(nodes + 1)] for x in range(nodes + 1)]
+    distMat = [['x' for x in range(nodes + 1)] for x in range(nodes + 1)]
 
     for i in graph.nodes:
         for j in graph.nodes:
-            if i != j and distMat[i][j] == -1:
+            if i != j and distMat[i][j] == 'x':
                 dist, path = dijkstra(graph, i, j)
-                for k in dist:
-                    if dist[i] < float('inf'):
+                for k in dist.keys():
+                    if dist[k] < float('inf'):
                         distMat[i][k] = dist[k]
                         distMat[k][i] = dist[k]
             elif i == j:
                 distMat[i][j] = 0
 
-    cluster = k_medoids(graph, num_cluster, distMat)
+    cluster = k_medoids(graph, num_cluster, distMat, 1000)
 
     pattern = ['x', 'y', 'z', 'w']
     for key, values in cluster.items():
@@ -66,4 +67,18 @@ if __name__ == '__main__':
             x, y = graph.nodes[k]['location']
             tmp[x][y] = c
 
-    print(np.rot90(tmp))
+    tmp = np.rot90(tmp)
+    m, n = tmp.shape
+    for i in range(m):
+        for j in range(n):
+            if tmp[i][j] == 'x':
+                print(Fore.BLUE + 'o', end=' ')
+            elif tmp[i][j] == 'y':
+                print(Fore.YELLOW + 'o', end=' ')
+            elif tmp[i][j] == 'z':
+                print(Fore.GREEN + 'o', end=' ')
+            elif tmp[i][j] == 'w':
+                print(Fore.RED + 'o', end=' ')
+            else:
+                print(Fore.BLACK + tmp[i][j], end=' ')
+        print()
